9a46943f904fd3aec6736ed431ee8e3aecef7e73
Raphda <raphprdn@mac.home>
2020-10-02 16:58:53 +0200

    fin td00bis

--- td00/src/main/java/fr/dauphine/ja/pardiniraphael/td00/PrimeCollection.java
+++ td00/src/main/java/fr/dauphine/ja/pardiniraphael/td00/PrimeCollection.java
@@ -10,26 +10,29 @@ import java.util.Random;
 public class PrimeCollection 
 {
 	private ArrayList<Integer> numbers;
-	
+
 	public PrimeCollection() {
 		this.numbers = new ArrayList<>();
 	}
-	
+
 	public void initRandom(int n, int m){
 		for(int i = 0; i < n; i++) {
 			int nombreAleatoire = (int)(Math.random() * (m + 1));
 			numbers.add(nombreAleatoire);
 		}
 	}
-	private boolean isPrime(int p) {
-		for(int i = 2; i < (int)Math.sqrt(p)+1; i++) {
-			if (p % i == 0) {
+	static boolean isPrime(int p) {
+		if(p==0 || p== 1) {
+			return false;
+		}
+		for (int i=2;i<=Math.sqrt(p);i++) {
+			if(p%i==0) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	private void printPrimes() {
 		for (int i : numbers) {
 			if (isPrime(i)) {
@@ -37,11 +40,11 @@ public class PrimeCollection
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		PrimeCollection liste = new PrimeCollection();
 		liste.initRandom(100, 2000);
 		liste.printPrimes();
-		
+
 	}
 }
--- td00/src/test/java/fr/dauphine/ja/pardiniraphael/td00/PrimeCollectionTest.java
+++ td00/src/test/java/fr/dauphine/ja/pardiniraphael/td00/PrimeCollectionTest.java
@@ -8,31 +8,54 @@ import junit.framework.TestSuite;
  * Unit test for simple App.
  */
 public class PrimeCollectionTest 
-    extends TestCase
+extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public PrimeCollectionTest( String testName )
-    {
-        super( testName );
-    }
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public PrimeCollectionTest( String testName )
+	{
+		super( testName );
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( PrimeCollectionTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite()
+	{
+		return new TestSuite( PrimeCollectionTest.class );
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testPrimeCollection()
-    {
-        assertTrue( true );
-    }
+	public boolean test0IsNotPrime() {
+		if(PrimeCollection.isPrime(0)==false) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean testTwoIsPrime(){
+		if(PrimeCollection.isPrime(2)==false) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean  test9IsNotPrime(){
+		if(PrimeCollection.isPrime(9)==false) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testPrimeCollection()
+	{
+		assertTrue(test0IsNotPrime());
+		assertTrue(testTwoIsPrime());
+		assertTrue(test9IsNotPrime());
+	}
 }
